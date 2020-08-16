bfce810737ed9d144f67602dd0fd9ccabf461648
leon-carvalho <leonardoscc15@gmail.com>
2020-08-15 21:32:20 -0300

    feat: loads Github profile data of the searched user

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Container, GithubLogo, SearchForm } from './styles';
 
+type handleSubmitTypes = (event: React.FormEvent) => void;
+
 const Header: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit: handleSubmitTypes = event => {
+    event.preventDefault();
+
+    navigate(`/${search.toLowerCase().trim()}`);
+  };
+
   return (
     <Container>
       <GithubLogo />
 
-      <SearchForm>
-        <input type="text" placeholder="Enter Username or Repo..." />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.currentTarget.value)}
+          placeholder="Enter Username or Repo..."
+        />
       </SearchForm>
     </Container>
   );
